7ef693dc8ad5831e1d3a5b202b7a00027c171214
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2024-12-29 11:15:19 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -41,6 +41,6 @@ chrome.runtime.onInstalled.addListener(() =>
     id: "",
     title: "View background image",
     contexts: ["page", "link", "image"],
-    documentUrlPatterns: ["https://*/*", "https://*/", "http://*/*", "http://*/"]
+    documentUrlPatterns: ["https://*/*", "https://*/", "http://*/*", "http://*/", "file://*/*", "file://*/"]
   })
 );
