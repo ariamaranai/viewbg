c18dcc880692d1f7fbfb0dc36456e5235c80bf04
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-03-03 23:14:05 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,50 +1,50 @@
-chrome.contextMenus.onClicked.addListener(async (_, tab) =>
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    world: (await chrome.contentSettings.javascript.get({
-      primaryUrl: tab.url
-    })).setting == "allow" ? "MAIN" : "ISOLATED",
-    func: () => {
-      let d = document;
-      let walker = d.createTreeWalker(d.activeElement, 1);
-      let w = innerWidth;
-      let h = innerHeight;
-      let urls = [];
-      let node;
-      while ((node = walker.nextNode())) {
-        if (node.checkVisibility()) {
-          let rect = node.getBoundingClientRect();
-          if (
-            rect.y < w &&
-            rect.bottom > 0 &&
-            rect.x < h &&
-            rect.right > 0 &&
-            rect.width > 99 &&
-            rect.height > 99
-          ) {
-            let styleMap = node.computedStyleMap();
-            let src = (
-              node.tagName == "IMG" && (
-                styleMap.get("position").toString() != "static" ||
-                styleMap.get("pointer-events").toString() == "none"
-              ) && node.src
-            ) || (
-              (styleMap = styleMap.get("background-image").toString())[3] == "(" &&
-                styleMap.slice(5, -2)
-            );
-            src && !urls.includes(src) && urls.push(src);
+chrome.contextMenus.onClicked.addListener(async (_, tab) => {
+  if (tab.url[0] != "c") {
+    try {
+      let results = await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          let d = document;
+          let walker = d.createTreeWalker(d.activeElement, 1);
+          let w = innerWidth;
+          let h = innerHeight;
+          let urls = [];
+          let node;
+          while ((node = walker.nextNode())) {
+            if (node.checkVisibility()) {
+              let rect = node.getBoundingClientRect();
+              if (
+                rect.y < w &&
+                rect.bottom > 0 &&
+                rect.x < h &&
+                rect.right > 0 &&
+                rect.width > 99 &&
+                rect.height > 99
+              ) {
+                let styleMap = node.computedStyleMap();
+                let src = (
+                  node.tagName == "IMG" && (
+                    styleMap.get("position").toString() != "static" ||
+                    styleMap.get("pointer-events").toString() == "none"
+                  ) && node.src
+                ) || (
+                  (styleMap = styleMap.get("background-image").toString())[3] == "(" &&
+                    styleMap.slice(5, -2)
+                );
+                src && !urls.includes(src) && urls.push(src);
+              }
+            }
           }
+          return urls;
         }
-      }
-      return urls;
-    }
-  }, results => {
-    let i = 0;
-    let urls = results[0].result;
-    while (i < urls.length)
-      chrome.tabs.create({ url: urls[i], index: ++i + tab.index });
-  })
-);
+      });
+      let i = 0;
+      let urls = results[0].result;
+      while (i < urls.length)
+        chrome.tabs.create({ url: urls[i], index: ++i + tab.index });
+    } catch (e) {}
+  }
+});
 chrome.runtime.onInstalled.addListener(() =>
   chrome.contextMenus.create({
     id: "",
