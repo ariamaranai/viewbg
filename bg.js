0caece2415fe7523464982ecd71037354e22467c
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-04-11 03:01:21 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,51 +1,56 @@
+chrome.runtime.onUserScriptMessage.addListener((m, s) => {
+  let i = 0;
+  while (i < m.length)
+    chrome.tabs.create({ url: m[i], index: ++i + s.tab.index });
+});
 chrome.contextMenus.onClicked.addListener((_, tab) =>
-  chrome.scripting.executeScript({
+  chrome.userScripts.execute({
     target: { tabId: tab.id },
-    func: () => {
-      let n = document;
-      let walker = n.createTreeWalker(n.activeElement, 1);
-      let w = innerWidth;
-      let h = innerHeight;
-      let urls = [];
-      while ((n = walker.nextNode())) {
-        if (n.checkVisibility()) {
-          let rect = n.getBoundingClientRect();
-          if (
-            rect.y < w &&
-            rect.bottom > 0 &&
-            rect.x < h &&
-            rect.right > 0 &&
-            rect.width > 99 &&
-            rect.height > 99
-          ) {
-            let styleMap = n.computedStyleMap();
-            let src = (
-              n.tagName == "IMG" && (
-                styleMap.get("position").toString() != "static" ||
-                styleMap.get("pointer-events").toString() == "none"
-              ) && n.src
-            ) || (
-              (styleMap = styleMap.get("background-image").toString())[3] == "(" &&
-                styleMap.slice(5, -2)
-            );
-            src && !urls.includes(src) && urls.push(src);
-          }
-        }
-      }
-      return urls;
+    js: [{ code:
+`{
+let n = document;
+let walker = n.createTreeWalker(n.activeElement, 1);
+let w = innerWidth;
+let h = innerHeight;
+let urls = [];
+while ((n = walker.nextNode())) {
+  if (n.checkVisibility()) {
+    let rect = n.getBoundingClientRect();
+    if (
+      rect.y < w &&
+      rect.bottom > 0 &&
+      rect.x < h &&
+      rect.right > 0 &&
+      rect.width > 99 &&
+      rect.height > 99
+    ) {
+      let styleMap = n.computedStyleMap();
+      let src = (
+        n.tagName == "IMG" && (
+          styleMap.get("position").toString() != "static" ||
+          styleMap.get("pointer-events").toString() == "none"
+        ) && n.src
+      ) || (
+        (styleMap = styleMap.get("background-image").toString())[3] == "(" &&
+          styleMap.slice(5, -2)
+      );
+      src && !urls.includes(src) && urls.push(src);
     }
-  }).then(results => {
-    let i = 0;
-    let urls = results[0].result;
-    while (i < urls.length)
-      chrome.tabs.create({ url: urls[i], index: ++i + tab.index });
+  }
+}
+chrome.runtime.sendMessage(urls);
+}`
+      }]
   }).catch(() => 0)
 );
-chrome.runtime.onInstalled.addListener(() =>
+chrome.runtime.onInstalled.addListener(() => (
+  chrome.userScripts.configureWorld({
+    messaging: !0
+  }),
   chrome.contextMenus.create({
     id: "",
     title: "View background image",
     contexts: ["page", "link", "image"],
     documentUrlPatterns: ["https://*/*", "http://*/*"]
   })
-);
+));
