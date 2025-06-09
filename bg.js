f508c3c5079784d78b96c9323505f578d638bed0
a <106607480+ariamaranai@users.noreply.github.com>
2025-06-09 11:52:56 +0900

    Add files via upload

--- bg.js
+++ bg.js
@@ -1,67 +1,29 @@
-chrome.contextMenus.onClicked.addListener((_, tab) =>
-  chrome.userScripts.execute({
-    target: { tabId: tab.id },
-    js: [{ code:
-`(() => {
-  let d = document;
-  let root = d.scrollingElement;
-  let { activeElement } = d;
-  let walker = d.createTreeWalker(activeElement, 1);
-  let x0 = root.scrollLeft;
-  let x1 = x0 + innerWidth;
-  let y0 = root.scrollTop;
-  let y1 = y0 + innerHeight;
-  let urls = [];
-  let n = walker.currentNode;
-
-  while (n) {
-    if (n.checkVisibility()) {
-      let r = n.getBoundingClientRect();
-      if ((r.y < y1 && y0 < r.bottom) || (r.x < x1 && x0 < r.right)) {
-        let styleMap = n.computedStyleMap();
-        let src = n.tagName == "IMG" && (
-          styleMap.get("position") + "" != "static" ||
-          styleMap.get("pointer-events") + "" == "none"
-        ) &&
-        n.currentSrc ||
-        (styleMap = styleMap.get("background-image") + "")[3] == "(" &&
-        styleMap.slice(5, -2);
-        src && urls.push(src);
+chrome.contextMenus.onClicked.addListener((info, { id, index }) => {
+  if (info.mediaType == "image") {
+    let { srcUrl } = info;
+    srcUrl &&
+    chrome.tabs.create({
+      url: srcUrl,
+      index: index + 1
+    });
+  } else
+    chrome.userScripts.execute({
+      target: { tabId: id },
+      js: [{ file: "main.js" }] 
+    }).then(results => {
+      let urls = results[0].result;
+      let i = urls.length;
+      if (i) {
+        while (
+          chrome.tabs.create({
+            url: urls[--i],
+            index: index + 1
+          }),
+          i
+        );
       }
-    }
-    n = walker.nextNode();
-  }
-
-  if (!urls.length) {
-    let { x, y, right, bottom } = activeElement.getBoundingClientRect();
-    let { images } = d;
-    let i = 0;
-    while (i < images.length) {
-      let image = images[i];
-      let r = image.getBoundingClientRect();
-      (image.naturalWidth > 1 || image.naturalHeight > 1) &&
-      r.y >= y && r.bottom <= bottom && r.x >= x && r.right <= right &&
-      urls.push(image.currentSrc);
-      ++i;
-    }
-  }
-
-  return urls;
-})();`
-    }]
-  }).then(results => {
-    let urls = results[0].result;
-    let i = urls.length;
-    if (i)
-      while (
-        chrome.tabs.create({
-          url: urls[--i],
-          index: tab.index + 1
-        }),
-        i
-      );
-  }).catch(() => 0)
-);
+    }).catch(() => 0)
+});
 chrome.runtime.onInstalled.addListener(() =>
   chrome.contextMenus.create({
     id: "",
---
+++ main.js
@@ -0,0 +1,55 @@
+(() => {
+  let d = document;
+  let root = d.scrollingElement;
+  let { activeElement } = d;
+  let walker = d.createTreeWalker(activeElement, 1);
+  let x0 = root.scrollLeft;
+  let x1 = x0 + innerWidth;
+  let y0 = root.scrollTop;
+  let y1 = y0 + innerHeight;
+  let urls = [];
+  let n = walker.currentNode;
+
+  while (n) {
+    if (n.checkVisibility()) {
+      let r = n.getBoundingClientRect();
+      if ((r.y < y1 && y0 < r.bottom) || (r.x < x1 && x0 < r.right)) {
+        let styleMap = n.computedStyleMap();
+        let src = n.tagName == "IMG" && (
+          styleMap.get("position") + "" != "static" ||
+          styleMap.get("pointer-events") + "" == "none"
+        ) &&
+        (n.srcset && n.srcset.split(" ").filter((v, i) => i % 2 == 0).at(-1) || n.currentSrc) ||
+        (styleMap = styleMap.get("background-image") + "")[3] == "(" &&
+        styleMap.slice(5, -2);
+        src && urls.push(src);
+      }
+    }
+    n = walker.nextNode();
+  }
+
+  if (!urls.length) {
+    let { x, y } = activeElement.getBoundingClientRect();
+    x ^= 0;
+    y ^= 0;
+    let { images } = d;
+    let minDist = 0;
+    let i = 0;
+    while (i < images.length) {
+      let img = images[i];
+      let r = img.getBoundingClientRect();
+      if (img.naturalWidth > 1 || img.naturalHeight > 1) {
+        let dist = r.x - x;
+        let distY = r.y - y;
+        dist >= 0 && distY >= 0 &&
+        (dist += distY, minDist == 0 || dist < minDist) && (
+          minDist = dist,
+          urls[0] = img.srcset && img.srcset.split(" ").filter((v, i) => i % 2 == 0).at(-1) || img.currentSrc
+        );
+      }
+      ++i;
+    }
+  }
+
+  return urls;
+})()
